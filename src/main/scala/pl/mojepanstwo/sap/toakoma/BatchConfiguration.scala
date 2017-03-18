162c8138037a09cb4ab0e663b08aa4cf91a5a037
mji <marek.jagielski@gmail.com>
2017-03-18 12:46:21 +0100

    begening

--- src/main/scala/pl/mojepanstwo/sap/toakoma/BatchConfiguration.scala
+++ src/main/scala/pl/mojepanstwo/sap/toakoma/BatchConfiguration.scala
@@ -1,28 +1,32 @@
 package pl.mojepanstwo.sap.toakoma
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.context.annotation.Configuration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import javax.activation.DataSource
 import org.springframework.context.annotation.Bean
+import org.springframework.batch.item.file.FlatFileItemReader
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfiguration {
   
     @Autowired
-    var jobBuilderFactory : JobBuilderFactory = _
+    var jobBuilderFactory: JobBuilderFactory = _
 
     @Autowired
-    var stepBuilderFactory : StepBuilderFactory = _
+    var stepBuilderFactory: StepBuilderFactory = _
 
     @Autowired
-    var dataSource : DataSource = _
+    var dataSource: DataSource = _
     
     @Bean
-    def reader() {
-      
+    def reader: String = {
+        val reader = "ccc"
+        println("Hello")
+        reader
     }
 }
