cc42b79cc3f674458f517d9cece4b44bcd78cd68
mji <marek.jagielski@gmail.com>
2017-03-19 17:19:17 +0100

    upgrade cli

--- src/main/scala/pl/mojepanstwo/sap/toakoma/Cli.scala
+++ src/main/scala/pl/mojepanstwo/sap/toakoma/Cli.scala
@@ -4,24 +4,37 @@ import org.apache.commons.cli.Options
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.{Option => CliOption}
 
 object Cli {
   
   var options: Options     = new Options()
   var args:    CommandLine = _
   
+  object JOB extends Enumeration {
+    type JOB = Value
+    val PDF_2_XML, 
+        ADD_2_DB = Value
+  }
+  
   {
-    options.addOption("f", "file",     true,  "Plik wejściowy")
-    options.addOption("m", "metadata", true,  "Plik informacyjny")
-    options.addOption("c", "config",   true,  "Plik konfiguracyjny")
-    options.addOption("h", "help",     false, "Ten tekst")
+    options.addOption(CliOption.builder("C")
+                               .longOpt("command")
+                               .desc("What to do")
+                               .hasArgs()
+                               .argName(JOB.values.mkString(" | "))
+                               .build())
+           .addOption("f", "file",     true,  "Plik wejściowy")
+           .addOption("m", "metadata", true,  "Plik informacyjny")
+           .addOption("c", "config",   true,  "Plik konfiguracyjny")
+           .addOption("h", "help",     false, "Ten tekst")
     
-    options.addOption("s", "status",            true, "Status aktu prawnego")
-    options.addOption("o", "data_ogloszenia",   true, "Data ogłoszenia")
-    options.addOption("w", "data_wydania",      true, "Data wydania")
-    options.addOption("z", "data_wejscia",      true, "Data wejścia w życie")
-    options.addOption("o", "organ_wydajacy",    true, "Organ wydający")
-    options.addOption("b", "organ_zobowiazany", true, "Organ zobowiązany")
+           .addOption("s", "status",            true, "Status aktu prawnego")
+           .addOption("o", "data_ogloszenia",   true, "Data ogłoszenia")
+           .addOption("w", "data_wydania",      true, "Data wydania")
+           .addOption("z", "data_wejscia",      true, "Data wejścia w życie")
+           .addOption("o", "organ_wydajacy",    true, "Organ wydający")
+           .addOption("b", "organ_zobowiazany", true, "Organ zobowiązany")
   }
   
   def parseArgs(args: Array[String]): Boolean = {
