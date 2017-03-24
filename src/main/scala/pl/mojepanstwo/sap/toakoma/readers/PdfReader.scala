49c986a0e2e6f2ae4227b2ad76ad00cc5d1e5e16
mji <marek.jagielski@gmail.com>
2017-03-24 21:39:06 +0100

    PdfReader . init

--- src/main/scala/pl/mojepanstwo/sap/toakoma/readers/PdfReader.scala
+++ src/main/scala/pl/mojepanstwo/sap/toakoma/readers/PdfReader.scala
@@ -14,12 +14,41 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.apache.pdfbox.text.PDFTextStripper
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.cos.COSDocument
+import java.io.File
+import java.io.FileInputStream
+import org.apache.pdfbox.pdfparser.PDFParser
+import org.apache.pdfbox.io.RandomAccessFile
+import java.io.IOException
 
-class PdfReader(filePath: String) extends ItemReader[String] {
+class PdfReader extends ItemReader[String] {
   
   val logger = LoggerFactory.getLogger(this.getClass())
-    
+  
+  def this(filePath: String) {
+    this()
+    var pdfStripper : PDFTextStripper = new PDFTextStripper()
+    var pdDoc : PDDocument = null
+    var cosDoc : COSDocument = null
+    val file : File = new File(filePath)
+    try {
+      val parser : PDFParser = new PDFParser(new RandomAccessFile(file, "rw"))
+      parser.parse();
+      cosDoc = parser.getDocument()
+      pdDoc = new PDDocument(cosDoc)
+      pdfStripper.setStartPage(1)
+      pdfStripper.setEndPage(5)
+      var parsedText : String = pdfStripper.getText(pdDoc)
+      System.out.println(parsedText);
+    } catch {
+      case e: IOException => e.printStackTrace
+    } 
+  }
+  
 	def read() : String = {
+
     null
   }
 }
