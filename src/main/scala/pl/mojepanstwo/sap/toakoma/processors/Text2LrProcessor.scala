cd03c5716617009c37ca9fe21dbc5ede9cb4c8d2
mji <marek.jagielski@gmail.com>
2017-04-30 12:19:29 +0200

    parse LR

--- src/main/scala/pl/mojepanstwo/sap/toakoma/processors/Text2LrProcessor.scala
+++ src/main/scala/pl/mojepanstwo/sap/toakoma/processors/Text2LrProcessor.scala
@@ -1,13 +1,22 @@
 package pl.mojepanstwo.sap.toakoma.processors
 
 import org.springframework.batch.item.ItemProcessor
+import pl.mojepanstwo.sap.toakoma.xml.{AkomaNtosoType, ObjectFactory}
 import pl.mojepanstwo.sap.toakoma.{IsapModel, Pdf}
+import javax.xml.bind.{JAXBContext, Marshaller}
 
 class Text2LrProcessor(pdf:Pdf.Value) extends ItemProcessor[IsapModel, IsapModel] {
 
   override def process(item:IsapModel): IsapModel = {
-    println(item)
-    println(pdf)
+    val input = item.texts(pdf)
+    val factory = new ObjectFactory()
+    val xmlType = factory.createAkomaNtosoType()
+    val output = factory.createAkomaNtoso(xmlType)
+
+    val context = JAXBContext.newInstance(classOf[AkomaNtosoType])
+    val marshaller = context.createMarshaller
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+    marshaller.marshal(output, System.out)
     item
   }
 }
