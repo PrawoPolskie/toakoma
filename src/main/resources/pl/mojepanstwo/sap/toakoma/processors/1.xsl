e855a05b707e7784639745086517e8ef792f2843
mji <marek.jagielski@gmail.com>
2017-05-02 14:40:07 +0200

    clean

---
+++ src/main/resources/pl/mojepanstwo/sap/toakoma/processors/1.xsl
@@ -0,0 +1,22 @@
+<xsl:stylesheet version="2.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:html="http://www.w3.org/1999/xhtml">
+
+    <xsl:template match="@*|node()" priority="0">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()"/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="comment()" priority="1"/>
+
+    <xsl:template match="html:head" priority="1"/>
+
+    <xsl:template match="html:span[starts-with(@class, '_ _')]"  priority="1"/>
+
+    <xsl:template match="html:span[@class='_ _0']" priority="2">
+        <xsl:value-of select="text()"/>
+    </xsl:template>
+
+
+</xsl:stylesheet>
--- src/main/scala/pl/mojepanstwo/sap/toakoma/processors/PreXsltProcessor.scala
+++ src/main/scala/pl/mojepanstwo/sap/toakoma/processors/PreXsltProcessor.scala
@@ -1,11 +1,34 @@
 package pl.mojepanstwo.sap.toakoma.processors
 
+import java.io.File
+import javax.xml.transform.stream.StreamSource
+
 import org.springframework.batch.item.ItemProcessor
 import pl.mojepanstwo.sap.toakoma.IsapModel
+import net.sf.saxon.s9api.{Processor, Serializer}
 
 class PreXsltProcessor extends ItemProcessor[IsapModel, IsapModel] {
 
+  val processor = new Processor(false)
+
+  val compiler = processor.newXsltCompiler()
+
+  val xsl_1 = compiler.compile(new StreamSource(classOf[PreXsltProcessor].getResourceAsStream("1.xsl"))).load
+  val xsl_2 = compiler.compile(new StreamSource(classOf[PreXsltProcessor].getResourceAsStream("2.xsl"))).load
+
   override def process(item:IsapModel): IsapModel = {
+    item.linksHtml.foreach { case (key, dirPath) =>
+      val input = new File(dirPath + "/output.html")
+      val source = processor.newDocumentBuilder.build(new StreamSource(input))
+
+      val out = processor.newSerializer(new File(dirPath + "/1.xml"))
+      out.setOutputProperty(Serializer.Property.METHOD, "xml")
+      out.setOutputProperty(Serializer.Property.INDENT, "yes")
+
+      xsl_1.setInitialContextNode(source)
+      xsl_1.setDestination(out)
+      xsl_1.transform()
+    }
     item
   }
 
