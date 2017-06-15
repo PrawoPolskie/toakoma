833338355a2256a9a70db5728c523dad61c4b5e8
mji <marek.jagielski@gmail.com>
2017-06-16 00:37:11 +0200

    IsapProcessorSpec

--- src/test/scala/pl/mojepanstwo/sap/toakoma/processors/IsapProcessorSpec.scala
+++ src/test/scala/pl/mojepanstwo/sap/toakoma/processors/IsapProcessorSpec.scala
@@ -1,10 +1,16 @@
 package pl.mojepanstwo.sap.toakoma.processors
 
 import pl.mojepanstwo.sap.toakoma._
+import org.jsoup.Jsoup
+import scala.io.Source
 
 class IsapProcessorSpec extends UnitSpec {
 
-  "A IsapProcessor" should "parse text" in {
+  "A IsapProcessor" should "parse html" in {
+    val document = Jsoup.parse(Source.fromResource("isap/WDU20170000001.html").mkString)
+    val model = new IsapProcessor().process(document)
+
+    assert(model.dziennik == Dziennik.DZIENNIK_USTAW)
   }
 
 }
