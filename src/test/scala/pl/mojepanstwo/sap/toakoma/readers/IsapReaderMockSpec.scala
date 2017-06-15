00b8d9de3fee8e7a5ff8700d0148366cb438cfeb
mji <marek.jagielski@gmail.com>
2017-06-15 23:49:33 +0200

    scala mocks

--- src/test/scala/pl/mojepanstwo/sap/toakoma/readers/IsapReaderMockSpec.scala
+++ src/test/scala/pl/mojepanstwo/sap/toakoma/readers/IsapReaderMockSpec.scala
@@ -1,12 +1,22 @@
 package pl.mojepanstwo.sap.toakoma.readers
 
 import pl.mojepanstwo.sap.toakoma._
+import scala.io.Source
+import org.jsoup.Jsoup
 
 class IsapReaderMockSpec extends IsapMockSpec {
 
-  "A Mock for IsapReader" should "read html" in {
-    val isapReaderMock = mock[IsapReader]
-//    val document       = isapReaderMock.read
+  val regexTrim = """(?m)\s+$"""
+
+  "A Mock for IsapReader" should "read html from file" in {
+    val isapReaderMock = stub[IsapReader]
+    (isapReaderMock.read _).when
+                           .returns(Jsoup.parse(Source.fromResource("isap/WDU20170000001.html").mkString))
+
+    val document = isapReaderMock.read
+
+    assert(document.toString.replaceAll(regexTrim, "").filter(_ >= ' ') ==
+           Source.fromResource("isap/WDU20170000001.html").mkString.replaceAll(regexTrim, "").filter(_ >= ' '))
   }
 
 }
